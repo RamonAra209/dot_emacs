2a86d43533fc12e519e78281ce4e81eae1af3fd0
Ramon Arambula <r_arambula@u.pacifc.edu>
2022-11-06 10:35:27 -0800

    Keymaps: Added section for window management

--- init.el
+++ init.el
@@ -317,20 +317,6 @@
 
 
 ;; Window Management
-(setq winum-keymap
-    (let ((map (make-sparse-keymap)))
-      (define-key map (kbd "C-`") 'winum-select-window-by-number)
-      (define-key map (kbd "C-²") 'winum-select-window-by-number)
-      (define-key map (kbd "M-0") 'winum-select-window-0-or-10)
-      (define-key map (kbd "M-1") 'winum-select-window-1)
-      (define-key map (kbd "M-2") 'winum-select-window-2)
-      (define-key map (kbd "M-3") 'winum-select-window-3)
-      (define-key map (kbd "M-4") 'winum-select-window-4)
-      (define-key map (kbd "M-5") 'winum-select-window-5)
-      (define-key map (kbd "M-6") 'winum-select-window-6)
-      (define-key map (kbd "M-7") 'winum-select-window-7)
-      (define-key map (kbd "M-8") 'winum-select-window-8)
-      map))
 (use-package winum :straight t :init (winum-mode))
 
 
@@ -529,6 +515,20 @@
 (leader-key-def "w" '(:ignore t :which-key "Window"))
 (leader-key-def "w c" 'evil-window-delete)
 (leader-key-def "w v" 'evil-window-vsplit)
+(setq winum-keymap
+    (let ((map (make-sparse-keymap)))
+      (define-key map (kbd "C-`") 'winum-select-window-by-number)
+      (define-key map (kbd "C-²") 'winum-select-window-by-number)
+      (leader-key-def "w 0" 'winum-select-window-0-or-10)
+      (leader-key-def "w 1" 'winum-select-window-1)
+      (leader-key-def "w 2" 'winum-select-window-2)
+      (leader-key-def "w 3" 'winum-select-window-3)
+      (leader-key-def "w 4" 'winum-select-window-4)
+      (leader-key-def "w 5" 'winum-select-window-5)
+      (leader-key-def "w 6" 'winum-select-window-6)
+      (leader-key-def "w 7" 'winum-select-window-7)
+      (leader-key-def "w 8" 'winum-select-window-8)
+      map))
 
 (leader-key-def "b" '(:ignore t :which-key "Buffer"))
 (leader-key-def "b k" 'image-kill-buffer)
